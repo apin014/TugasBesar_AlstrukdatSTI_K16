ef98acfb66833e62785f219dc1f3fae7d6fc3fd3
verawatisimatupang <88297371+verawatisimatupang@users.noreply.github.com>
2021-11-23 01:18:30 +0700

    Update skill.h

--- src/skill.h
+++ src/skill.h
@@ -1,24 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"boolean.h"
+#include "boolean.h"
+#include "listlinier.h"
+#include "player.h"
 
 #ifndef skill_h
 #define skill_h
 
-const char *skillNames[9];
-skillNames[1] = "Pintu Ga Ke Mana Mana";
-skillNames[2] = "Mesin Waktu";
-skillNames[3] = "Baling Baling Jambu";
-skillNames[4] = "Cermin Pengganda";
-skillNames[5] = "Senter Pembesar Hoki";
-skillNames[6] = "Senter Pengecil Hoki";
-skillNames[7] = "Mesin Penukar Posisi";
-skillNames[8] = "Teknologi Gagal";
+const char *skillNames[] = {"Pintu Ga Ke Mana Mana", "Mesin Waktu", 
+"Baling Baling Jambu", "Cermin Pengganda",
+"Senter Pembesar Hoki", "Senter Pengecil Hoki",
+"Mesin Penukar Posisi","Teknologi Gagal"};
 
-/*di sini deklarasiin smua fungsi2 yang bakal dipake berkaitan sama skill
-randomizer pindah sini, bedanya sekarang langsung insert elemen ke list skill punya player 
-terus fungsi2 lainnya, kayak buat print skill yang dimiliki player, pake skill, buang skill, dll
-pasti bakal nyambung sama player dan buff
-*/
+void RandomSkill(int number);
+/* Menampilkan skill dari hasil random */
+
+void TotalSkill(List *L, int X);
+/* Mengecek jumlah skill pemain */
+
+void SKILL(Player P, int input);
+/* Menampilkan daftar skill yang dimiliki dan menanyakan 
+apakah pemain ingin menggunakan suatu skill. */
+
+void UseSkill(Player P, int *input);
+/* Menggunakan skill pemain jika memasukkan nilai positif */
+
+void RemoveSkill(Player *P, int *input);
+/* Membuang skill pemain jika memasukkan nilai negatif */
+
+char SearchValue(List L, int X);
+/* Mencari value dari suatu array dengan indeksnya */
 
 #endif
